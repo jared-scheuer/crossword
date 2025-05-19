bd16f428c0127411ad56bb43a9f1ba3899cbbd7d
jared-scheuer <jared.scheuer@gmail.com>
2025-05-19 16:41:23 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("v1").then(cache => {
+      return cache.addAll([
+        "/index_top_banner_cluebox_reverted_final.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
